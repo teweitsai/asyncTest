93b8f039ce1eefd901012cb7198607a7b9505520
Te-Wei Tsai <ttsai@lsst.org>
2019-06-04 14:31:58 -0700

    Add the salobj structure.

--- testAsync4.py
+++ testAsync4.py
@@ -13,7 +13,7 @@ async def mygen2(alist):
     while len(alist) > 0:
         c = len(alist) - 1
         print(alist.pop(c))
-        time.sleep(2)
+        await asyncio.wait(2)
 
 
 async def waitLongTime(s):
@@ -27,12 +27,51 @@ async def doWork():
     await mygen2([1])
     await waitLongTime(4.0)
 
+async def doWork1():
+    op1 = mygen2(["ss"])
+    await op1
+    op2 = mygen2([1])
+    await op2
+    op3 = waitLongTime(4.0)
+    await op3
+
 
 async def doWork2():
     op1 = mygen2(["ss"])
     op2 = mygen2([1])
     op3 = waitLongTime(4.0)
     await asyncio.wait([op3, op2, op1])
+    print("done")
+
+
+async def timeout(timeInSec):
+    await asyncio.sleep(timeInSec)
+
+def getCommandAck():
+    return False
+
+async def checkForCommandAck():
+    while True:
+        if getCommandAck():
+            break
+        else:
+            await asyncio.sleep(0.1)
+
+
+async def issueCommandGetResponse(timeInSec):
+    issueCommandViaSAL()
+    timeoutTask = timeout(timeInSec)
+    getAckTask = checkForCommandAck()
+    await asyncio.waitAny([timeoutTask, getAckTask])
+    if getAckTask.is_done:
+        timeoutTask.close()
+        doThingsForCompletedCommand()
+    elif timeoutTask.is_done:
+        getAckTask.close()
+        throwTimeoutError()
+
+
+
 
 
 if __name__ == "__main__":
